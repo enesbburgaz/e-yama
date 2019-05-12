38121df2dc4983f611f18fefdef541335bf2ce26
enesbburgaz <enesbburgaz@gmail.com>
2019-05-12 22:56:02 +0300

    urun detayi eklendi

--- e-yama/Controllers/ProductController.cs
+++ e-yama/Controllers/ProductController.cs
@@ -1,11 +1,20 @@
+using System.Linq;
+using System.Threading.Tasks;
+using e_yama.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace e_yama.Controllers
 {
     public class ProductController : Controller
     {
-        public IActionResult Index(){
-            return View();
+        private readonly EfDataContext _context;
+        public ProductController(EfDataContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index(int id){
+            return View(await _context.Products.FindAsync(id));
         }
     }
 }
